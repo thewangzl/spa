57dd4deb844985b7a1ebb39fe2ecbf446815e11b
thewangzl <thewangzl@gmail.com>
2016-04-17 09:04:33 +0800

    common crud

--- spa/app.js
+++ spa/app.js
@@ -34,28 +34,28 @@ app.get('/',function(request, response){
 	response.redirect('/spa.html');
 });
 
-app.all('/user/*?', function(request, response, next){
+app.all('/obj_type/*?', function(request, response, next){
 	response.contentType('json');
 	next();
 });
 
-app.get('/user/list', function(request, response){
-	response.send({title : 'user list'});
+app.get('/obj_type/list', function(request, response){
+	response.send({title : request.params.obj_type + ' list'});
 });
 
-app.post('/user/create', function(request, response){
-	response.send({title : 'user created'});
+app.post('/obj_type/create', function(request, response){
+	response.send({title : request.params.obj_type + ' created'});
 });
 
-app.get('/user/read/:id([0-9]+)', function(request, response){
+app.get('/:obj_type/read/:id([0-9]+)', function(request, response){
 	response.send({
-		title : 'user with id ' + request.params.id + ' found'
+		title : request.params.obj_type + ' with id ' + request.params.id + ' found'
 	});
 });
 
-app.post('/user/update/:id([0-9]+)', function(request, response){
+app.post('/obj_type/update/:id([0-9]+)', function(request, response){
 	response.send({
-		title : 'user with id ' + request.params.id + ' updated'
+		title : request.params.obj_type + ' with id ' + request.params.id + ' updated'
 	});
 });
 
